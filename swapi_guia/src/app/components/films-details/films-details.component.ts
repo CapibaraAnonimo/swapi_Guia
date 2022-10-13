f51d63238eb4a07371a155e07f7ac3285a3b4615
CapibaraNonimo <adrianarnaizcano@gmail.com>
2022-10-13 13:43:28 +0200

    ya está más o menos funcional la página de detalle de película

--- swapi_guia/src/app/components/films-details/films-details.component.ts
+++ swapi_guia/src/app/components/films-details/films-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FilmsService} from "../../services/films.service";
+import {Film} from "../../interfaces/films.interface";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-films-details',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./films-details.component.css']
 })
 export class FilmsDetailsComponent implements OnInit {
+  film: any;
+  id: any;
 
-  constructor() { }
+  constructor(private filmService: FilmsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+    this.filmService.getFilmById(Number(this.id)).subscribe(response => {
+      this.film = response;
+    })
+  }
+
+  saveImg(film: Film){
+    let nameFilm = film.url.split("/")[5]
+    return `https://starwars-visualguide.com/assets/img/films/${nameFilm}.jpg`
   }
 
 }
--- swapi_guia/src/app/services/films.service.ts
+++ swapi_guia/src/app/services/films.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { FilmsResponse } from '../interfaces/films.interface';
+import {Film, FilmsResponse} from '../interfaces/films.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +12,9 @@ export class FilmsService {
   constructor(private http: HttpClient) { }
   getFilms(): Observable<FilmsResponse> {
     return this.http.get<FilmsResponse>(`${environment.apiBaseUrl}/films/`);
+  }
+
+  getFilmById(id: number): Observable<Film> {
+    return this.http.get<Film>(`${environment.apiBaseUrl}/films/${id}`);
   }
 }
